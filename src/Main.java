0a69bd734d3fcff2c0af3d00e59a1d1f9022bfce
Edisolt5498 <edisolt5498@gmail.com>
2023-02-10 08:17:37 +0300

    /

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         for (int i : Range.fromTo(0, 10)) {
             System.out.println(i);
         }
@@ -11,30 +11,25 @@ public class Main {
             return new Range(from, to);
         }
         private Range (int start, int end) {
-            System.out.println("Range");
             this.start = start;
             this.end = end;
         }
 
         @Override
         public Iterator iterator() {
-            System.out.println("Range.iterator");
             return new Iterator(start);
         }
         public class Iterator implements java.util.Iterator<Integer> {
             int current;
             public Iterator (int current) {
-                System.out.println("Iterator");
                 this.current = current;
             }
             @Override
             public boolean hasNext() {
-                System.out.println("hasNext");
                 return current <= end;
             }
             @Override
             public Integer next() {
-                System.out.println("next");
                 return current++;
             }
         }
